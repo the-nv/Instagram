2ef2be610848f5e603975fe6c5dacada43c9cc73
the-nv <n.verma9252@gmail.com>
2020-09-10 15:02:30 +0530

    reduced redundancy

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+    before_action :set_post, only: [:show, :edit, :update, :destroy]
+
     def index
         @posts = Post.all
     end
 
     def show
-        @post = Post.find(params[:id])
     end
 
     def new
@@ -12,32 +13,35 @@ class PostsController < ApplicationController
     end
 
     def edit
-        @post =Post.find(params[:id])
     end
 
     def create
-        @post = Post.create(post_params)
-
-        redirect_to posts_path
+        if @post = Post.create(post_params)
+            redirect_to posts_path
+        else
+            render :new
+        end
     end
 
     def update
-        @post =Post.find(params[:id])
-
-        @post.update(post_params)
-
-        redirect_to post_path(@post)
+        if @post.update(post_params)
+            redirect_to post_path(@post)
+        else
+            render :edit
+        end
     end
 
     def destroy
-        @post = Post.find(params[:id])
-
         @post.destroy
 
         redirect_to posts_path
     end
 
     private
+        def set_post
+            @post = Post.find(params[:id])
+        end
+
         def post_params
             params.require(:post).permit(:image, :caption)
         end
